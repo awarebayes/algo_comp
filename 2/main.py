ea1a846ac38215bc086bccd339f49694315aef91
Mikhail S <awarebayes@gmail.com>
2022-03-28 19:44:06 +0300

    я съел деда

---
+++ 2/main.py
@@ -0,0 +1,81 @@
+import numpy as np
+
+from io_table import *
+import streamlit as st
+import pandas as pd
+from numpy_newton import newton_3d, newton_3d_alt
+
+
+def main():
+
+    table_type = st.selectbox("Тип функции", ["из таблицы", "своя"])
+    func = None
+    if table_type == "из таблицы":
+        table = parse_table("data.txt")
+    else:
+        xs = st.number_input("Введите x_start: ", -100.0, 100.0, 1.0, step=1.0, format="%.5f")
+        xe = st.number_input("Введите x_end: ", -100.0, 100.0, 10.0, step=1.0, format="%.5f")
+        xst = int(st.number_input("Введите кол во узлов x: ", 1, 100, 10))
+
+        ys = st.number_input("Введите y_start: ", -100.0, 100.0, 1.0, step=1.0, format="%.5f")
+        ye = st.number_input("Введите y_end: ", -100.0, 100.0, 10.0, step=1.0, format="%.5f")
+        yst = int(st.number_input("Введите кол во узлов y: ", 1, 100, 10))
+
+        zs = st.number_input("Введите z_start: ", -100.0, 100.0, 1.0, step=1.0, format="%.5f")
+        ze = st.number_input("Введите z_end: ", -100.0, 100.0, 10.0, step=1.0, format="%.5f")
+        zst = int(st.number_input("Введите кол во узлов z: ", 1, 100, 10))
+
+        st.subheader("Expression")
+        expression = st.text_input("Введите выражение", "x + y / z")
+        func = eval(f"lambda x, y, z: {expression}")
+
+        table = get_table(xs, xe, xst, ys, ye, yst, zs, ze, zst, expression)
+
+    st.header("Table")
+
+    #for k in range(len(table[2])):
+    #    st.write("z =", int(table[2][k]))
+    #    st.write(np.array(table[3][k]))
+
+    st.header("Newton")
+    x = st.number_input("Введите аргумент x: ", -100.0, 100.0, 1.0, step=1.0, format="%.5f")
+    y = st.number_input("Введите аргумент y: ", -100.0, 100.0, 1.0, step=1.0, format="%.5f")
+    z = st.number_input("Введите аргумент z: ", -100.0, 100.0, 1.0, step=1.0, format="%.5f")
+
+    # 0.152, 0.131, 1.43
+
+    nx = int(st.number_input("Введите степень аппроксимиляции nx: ", 0, 10, 3))
+    ny = int(st.number_input("Введите степень аппроксимиляции ny: ", 0, 10, 3))
+    nz = int(st.number_input("Введите степень аппроксимиляции nz: ", 0, 10, 3))
+
+    if table_type == "Из таблицы":
+       result = newton_3d(table, nx + 1, ny + 1, nz + 1, x, y, z)
+    else:
+        xs = np.linspace(xs, xe, xst)
+        ys = np.linspace(ys, ye, yst)
+        zs = np.linspace(zs, ze, zst)
+        result = newton_3d_alt((xs, ys, zs), func, (nx+1, ny+1, nz+1), (x, y, z))
+
+    st.subheader("Result")
+    st.write(f"Answer: {result}")
+
+    if func:
+        st.write("Actual:", func(x, y, z))
+    st.subheader("All interpolations:")
+
+    show_interpolations = st.checkbox("Show interpolations", False)
+
+    if show_interpolations:
+        for i_nz in range(1, nz + 1):
+            st.write(f"nz = {i_nz}")
+            df = {"nx": list(range(1, nx + 1))}
+            for i_ny in range(1, ny + 1):
+                df[f"ny={i_ny}"] = []
+                for i_nx in range(1, nx + 1):
+                    result = newton_3d(table, i_nx + 1, i_ny + 1, i_nz + 1, x, y, z)
+                    df[f"ny={i_ny}"].append(result)
+            st.dataframe(pd.DataFrame(df).set_index("nx"))
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2/numpy_newton.py
@@ -0,0 +1,69 @@
+import numpy as np
+import streamlit as st
+
+
+def y_newt(i, k, xs, ys) -> float:
+    if i + k >= len(xs):
+        return 0
+    elif k == 0:
+        return ys[i]
+    else:
+        return (y_newt(i + 1, k - 1, xs, ys) - y_newt(i, k - 1, xs, ys)) / (
+                xs[i + k] - xs[i]
+        )
+
+
+def topk_closest(x, xs, k):
+    closest = np.abs(xs - x).argsort()
+    return np.sort(closest[:k])
+
+
+def newton(x, xs, ys, power):
+    topk = topk_closest(x, xs, k=power)
+    xs = xs[topk]
+    ys = ys[topk]
+
+    lp = y_newt(0, 0, xs, ys)
+    for k in range(1, power):
+        lp += y_newt(0, k, xs, ys) * np.prod(x - xs[:k])
+    return lp
+
+
+def newton_3d(data, nx, ny, nz, xp, yp, zp):
+    xs = np.array(data[0])
+    ys = np.array(data[1])
+    zs = np.array(data[2])
+    tensor = np.array(data[3])
+
+    z_values = np.zeros(len(zs))
+    y_values = np.zeros(len(ys))
+
+    for i in range(tensor.shape[0]):
+        for j in range(tensor.shape[1]):
+            x_values = tensor[i][j]
+            y_values[j] = newton(xp, xs, x_values, nx)
+        z_values[i] = newton(yp, ys, y_values, ny)
+    ans = newton(zp, zs, z_values, nz)
+    return ans
+
+
+def newton_3d_alt(axis, func, powers, point):
+    xs, ys, zs = axis
+    x_pow, y_pow, z_pow = powers
+    x, y, z = point
+
+    x_grid, y_grid, z_grid = np.meshgrid(*axis, indexing='ij')
+    tensor = func(x_grid, y_grid, z_grid)
+    tensor = np.swapaxes(tensor, 0, 2)
+
+    z_values = np.zeros(tensor.shape[0])
+    y_values = np.zeros(tensor.shape[1])
+
+    for i in range(tensor.shape[0]):
+        for j in range(tensor.shape[1]):
+            x_values = tensor[i][j]
+            y_values[j] = newton(x, xs, x_values, x_pow)
+        z_values[i] = newton(y, ys, y_values, y_pow)
+    ans = newton(z, zs, z_values, z_pow)
+    return ans
+
